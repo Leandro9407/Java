64d9b129e70ade9efcb35c07432ae6a5c640160b
alzate <fullm94@gmail.com>
2024-04-24 17:10:55 -0500

    Ejercicio6 de condicionales 3

---
+++ 08-JAVA - Condicionales 3/Ejercicio6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ejercicio6{
+    public static void main(String[] args){
+
+        Scanner teclado_numero=new Scanner (System.in);
+
+        
+
+
+
+    }
+}
